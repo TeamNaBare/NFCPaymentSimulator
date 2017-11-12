54b4a31ad1af5824128ca752e2431d579d3ace4d
Viktor <rybeckyv@gmail.com>
2017-11-12 16:23:49 +0100

    update MerchandiseFragment >> pridané Android Room pre prácu s DB (build.gradle), vylistovanie zoznamu produktov (RecyclerView, ProductsAdapter ), model Product - polozky automatu + nejaký pomocný interface pre dotazy na produkty

--- app/src/main/java/com/cvika/mobv/nfcpaymentsimulator/fragments/MerchandiseFragment.java
+++ app/src/main/java/com/cvika/mobv/nfcpaymentsimulator/fragments/MerchandiseFragment.java
@@ -39,9 +39,6 @@ public class MerchandiseFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_merchandise, container, false);
 
-
-
-
         context = this.getActivity().getApplicationContext();
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.productsRecyclerView);
